b6d87d70ea8a6844cfc080f0a0c07a4aacb64e6a
HarshalPatil <patilharshal2282@gmail.com>
2025-07-08 20:24:45 +0530

    Serve frontend to backend

--- Server/server.js
+++ Server/server.js
@@ -2,16 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
 // Route imports
 const contactRoutes = require("./routes/contactRoutes");
 const projectRoutes = require("./routes/projects");
 const skillsRoutes = require("./routes/skills");
 const { router: authRouter, verifyToken } = require("./routes/auth");
+console.log("Registered routes:");
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const _dirname = path.resolve();
 
 // Middleware
 app.use(cors());
@@ -22,6 +26,15 @@ app.use("/api/contact", contactRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/auth", authRouter);
 app.use("/api/skills", skillsRoutes);
+
+app.use(express.static(path.join(_dirname, "..","client/build")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "..","client","build", "index.html"));
+})
+
+
+
+
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
